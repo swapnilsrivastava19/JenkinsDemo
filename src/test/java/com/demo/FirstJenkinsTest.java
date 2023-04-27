d6d188d13af9c781cd6add050adf8c72ed0727f5
swapnil <swapnilsrivastava92.ss@gmail.com>
2023-04-28 01:53:50 +0530

    parameter added

--- src/test/java/com/demo/FirstJenkinsTest.java
+++ src/test/java/com/demo/FirstJenkinsTest.java
@@ -4,14 +4,25 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class FirstJenkinsTest {
 	
+	@Parameters("Browser")
 	@Test
-	public void firstTest() {
-		WebDriver driver=new ChromeDriver();
+	public void firstTest(String browserName) {
+		System.out.println("Parameter value is "+browserName);
+		WebDriver driver=null;
+		if(browserName.equalsIgnoreCase("chrome")) {
+			driver=new ChromeDriver();
+		}
+		else if(browserName.equalsIgnoreCase("edge")){
+			driver=new EdgeDriver();
+		}
+		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		 driver.get("https://tutorialsninja.com/demo/");
